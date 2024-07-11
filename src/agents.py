add special a_sampled vec for LSTM1. fix losses scaling. a2 sampl return

src/agents.py
@@ -2,7 +2,7 @@ from __future__ import division
 import torch
 import torch.nn.functional as F
 from torch.autograd import Variable
-
+import numpy as np
 
 ## STOP rewrite agetnt with respect to hidden_states. 
 ## add Star features or switches or separate class
@@ -48,7 +48,7 @@ class Agent(object):
         self.a2_prev = torch.zeros((1,4)).to("cuda:"+str(gpu_id))
         
         self.prev_action_logits = torch.zeros((1,6)).to("cuda:"+str(gpu_id))
-        self.prev_action1_logits = torch.zeros((1,6)).to("cuda:"+str(gpu_id))
+        self.prev_action_sampled = torch.zeros((1,6)).to("cuda:"+str(gpu_id))
         self.prev_action2_logits = torch.zeros((1,8)).to("cuda:"+str(gpu_id))
 
 
@@ -119,7 +119,7 @@ class Agent(object):
         with torch.autograd.set_detect_anomaly(True):
             kld1, x_restored1, v1, a1, self.hx1, self.cx1, s1, S1 = self.model1((Variable(
                 self.state.unsqueeze(0)),self.hx1, self.cx1, self.prev_action_logits.detach(),
-                                                            self.prev_action1_logits.detach()))
+                                                            self.prev_action_sampled.detach()))
             
 #             self.S1_runningmean = self.S1_runningmean*self.gamma1 + S1.detach()*(1-self.gamma1)
             
@@ -146,7 +146,7 @@ class Agent(object):
             self.train_episodes_run_2+=1
             
             self.prev_action_logits = a_base
-            self.prev_action1_logits = a1
+            
             self.prev_action2_logits = a2
 
 #             restoration_loss1 = self.w_restoration * (x_restored1 - self.state.unsqueeze(0).detach()).pow(2).sum()/ self.batch_size
@@ -159,6 +159,13 @@ class Agent(object):
         
         prob1, log_prob1, action1, entropy1 = self.get_probs(a)
         self.actions.append(action1)
+        
+        sampled_special_vector = np.ones_like(a.squeeze().detach().cpu().numpy())
+        sampled_special_vector = sampled_special_vector*(-1/(np.sqrt(6*5)))
+        sampled_special_vector[action1.squeeze().item()] = np.sqrt(5/6)
+        sampled_special_vector = torch.from_numpy(sampled_special_vector).unsqueeze(0)
+        self.prev_action_sampled = sampled_special_vector #for action done in env
+        
         
         prob_base, log_prob_base, action_base, entropy_base = self.get_probs(a_base)
         
@@ -277,7 +284,7 @@ class Agent(object):
                 self.cx2 = self.cx2.data
             
             kld1, x_restored1, v1, a1, self.hx1, self.cx1, s1, S1 = self.model1((Variable(
-                self.state.unsqueeze(0)),self.hx1, self.cx1, self.prev_action_logits.detach(), self.prev_action1_logits.detach()))
+                self.state.unsqueeze(0)),self.hx1, self.cx1, self.prev_action_logits.detach(), self.prev_action_sampled.detach()))
             
 #             self.S1_runningmean = self.S1_runningmean*self.gamma1 + S1.detach()*(1-self.gamma1)
 #             self.S1_runningmean = self.S1_runningmean*self.gamma1 + S1.detach()*(1-self.gamma1)
@@ -293,11 +300,20 @@ class Agent(object):
 #             a = alpha1*a1 + alpha2*a_base
             
             self.prev_action_logits = a_base
-            self.prev_action1_logits = a1
+#             self.prev_action1_logits = a1
             self.prev_action2_logits = a2
                 
         prob = F.softmax(a, dim=1)
         action = prob.max(1)[1].data.cpu().numpy()
+        
+        action1 = action
+        sampled_special_vector = np.ones_like(a.squeeze().detach().cpu().numpy())
+        sampled_special_vector = sampled_special_vector*(-1/(np.sqrt(6*5)))
+        sampled_special_vector[action1.squeeze().item()] = np.sqrt(5/6)
+        sampled_special_vector = torch.from_numpy(sampled_special_vector).unsqueeze(0)
+        self.prev_action_sampled = sampled_special_vector #for action done in env
+        
+        
         state, self.reward, self.done, self.info = self.env.step(action[0])
         self.state = torch.from_numpy(state).float()
         self.original_state = state

src/models.py
@@ -146,9 +146,13 @@ class Oracle2(nn.Module):
 class Actor2(nn.Module):
     def __init__(self, args, device = "cpu"):
         super(Actor2, self).__init__()
-        self.a2 = nn.Linear(64*5*5, 8)
-        self.a2.apply(init_base) #.weight.data = #norm_col_init(self.a2.weight.data,1)
+        self.a2_mean = nn.Linear(64*5*5, 8)
+        self.a2_mean.apply(init_base) #.weight.data = #norm_col_init(self.a2.weight.data,1)
 #         self.action_std = nn.Linear(64*5*5, 8)
+
+        self.a2_std = nn.Linear(64*5*5, 8)
+        self.a2_std.apply(init_base)
+        
         self.gamma2 = float(args["Training"]["initial_gamma2"])
 
         self.a_mean=0
@@ -161,11 +165,15 @@ class Actor2(nn.Module):
 
     def forward(self, S):
         
-        a_mean = self.a2(T.clone(S)) #prediction form network
+        a_mean = self.a2_mean(T.clone(S)) #prediction form network
+        a_log_std = self.a2_std(T.clone(S)) #prediction form network
 
-        kl2 = 0 
+        kl2 = -0.5*(1 + 2*a_log_std - a_mean**2 - T.exp(2*a_log_std)).mean()
 
-        a = a_mean# + T.exp(a_log_std) * z_t #self.z_EMA_t
+        z_t = self.N.sample(a_mean.shape)
+        self.z_EMA_t=self.z_EMA_t*self.gamma2 + z_t*np.sqrt(1-self.gamma2**2)
+
+        a = a_mean + T.exp(a_log_std) * self.z_EMA_t
 
         log_probs2 = None #dist.log_prob(a)
         return a, None, log_probs2, kl2

src/train.py
@@ -170,7 +170,7 @@ def train(rank, args, shared_model, optimizer, env_conf,lock,counter, num, main_
             state = player.state
             kld1, x_restored1, v1, a1, hx1, cx1, s1, S1 = player.model1((Variable(
             state.unsqueeze(0)), player.hx1, player.cx1, player.prev_action_logits.detach(),
-                                                                         player.prev_action1_logits.detach()))
+                                                                         player.prev_action_sampled.detach()))
             
             kld2, x_restored2, v2, a2, a_base, hx2, cx2, s2, S2, ents, logprobs2, kld_actor2 = player.model2((S1.detach(), player.hx2,player.cx2, player.prev_action2_logits.detach())
                                                                  )
@@ -192,7 +192,7 @@ def train(rank, args, shared_model, optimizer, env_conf,lock,counter, num, main_
 
             loss1 += args["Training"]["w_kld"]*kld_loss1
             loss2 += args["Training"]["w_kld"]*kld_loss2
-            loss2 += 0.000*kld_loss_actor2 #args["Training"]["w_kld"]*
+            loss2 += args["Training"]["w_kld"]*kld_loss_actor2 #args["Training"]["w_kld"]*
 
             loss1 += args["Training"]["w_MPDI"]*MPDI_loss1
             loss2 += args["Training"]["w_MPDI"]*MPDI_loss2
@@ -374,8 +374,8 @@ def train_A3C_united(player, V_last1, V_last2, S_last1, S_last2, tau, gamma1, ga
         advantage1 = V_last1 - player.values1[i]
         value_loss1 = value_loss1 + 0.5 * advantage1.pow(2)
         
-    kld_loss1 = kld_loss1/len(player.klds1)
-    kld_loss2 = kld_loss2/len(player.klds2)
+    kld_loss1 = kld_loss1
+    kld_loss2 = kld_loss2
     if not (type(S_loss1)==int):
         S_loss1 = torch.sum(S_loss1) 
     if not (type(S_loss2)==int):